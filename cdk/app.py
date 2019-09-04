af0f907f81a2efbf9b891e4554ac0a728e2f460d
James Seward <sewardjm@amazon.com>
2019-09-04 18:01:04 +0100

    Add network_stack

--- cdk/app.py
+++ cdk/app.py
@@ -4,11 +4,13 @@ from aws_cdk import core
 
 from cdk.developer_stack import DeveloperStack
 from cdk.webapplication_stack import WebApplicationStack
+from cdk.network_stack import NetworkStack
 
 
 app = core.App()
 
 developerToolStack = DeveloperStack(app, "MythicalMysfits-DeveloperTools")
 WebApplicationStack(app, "MythicalMysfits-WebApplication")
+NetworkStack(app, "MythicalMysfits-NetworkStack")
 
 app.synth()
