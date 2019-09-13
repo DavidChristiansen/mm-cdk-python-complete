cebfe4276081fa5e641cd274688dc67af48e4ab4
James Seward <sewardjm@amazon.com>
2019-09-13 12:12:59 -0700

    Add ECR stack

--- cdk/app.py
+++ cdk/app.py
@@ -5,6 +5,7 @@ from aws_cdk import core
 from cdk.developer_stack import DeveloperStack
 from cdk.webapplication_stack import WebApplicationStack
 from cdk.network_stack import NetworkStack
+from cdk.ecr_stack import EcrStack
 
 
 app = core.App()
@@ -12,5 +13,6 @@ app = core.App()
 developerToolStack = DeveloperStack(app, "MythicalMysfits-DeveloperTools")
 WebApplicationStack(app, "MythicalMysfits-WebApplication")
 NetworkStack(app, "MythicalMysfits-NetworkStack")
+EcrStack(app, "MythicalMysfits-EcrStack")
 
 app.synth()
